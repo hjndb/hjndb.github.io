38f77700427017a9cfc8b35ca451c321012e4d38
hjndb <160691197+hjndb@users.noreply.github.com>
2024-03-07 18:47:17 +0700

    Add files via upload
    thiệp mừng

---
+++ prensent.js
@@ -0,0 +1,46 @@
+
+var to = 'Bạn';
+var gift_url = 'https://www.youtube.com/watch?v=FmQbFdsngMA';
+var gift_image_url = 'mấy bây.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('crad').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
